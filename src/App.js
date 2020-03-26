635d3c7168149e7df256741c9a40fd654ceced25
Lion Porphirio <lion.porphirio@gmail.com>
2020-03-25 22:15:33 -0300

    Back-end da aplicação

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello OmniStack
+          Izzet é ruim
         </p>
         <a
           className="App-link"
